b58221ac30a2e53ee110ea437afc3af74c765405
JanArvik <JanArvik01@gmail.com>
2022-08-26 18:05:54 -0400

    Update rollcall.js

--- commands/rollcall.js
+++ commands/rollcall.js
@@ -4,14 +4,24 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('rollcall')
         .setDescription('Pings for roll call')
-        .addIntegerOption(option =>
+        .addIntegerOption((option) =>
             option.setName('day')
-                .setDescription('day of the session 0-6')
-                .setRequired(true)),
+                .setDescription('Day of the session')
+                .setRequired(true)
+                .addChoices(
+                    {name: 'Sunday', value: 0},
+                    {name: 'Monday', value: 1},
+                    {name: 'Tuesday', value: 2},
+                    {name: 'Wednesday', value: 3},
+                    {name: 'Thursday', value: 4},
+                    {name: 'Friday', value: 5},
+                    {name: 'Saturday', value: 6}
+                )),
+                    
 
     async execute(interation) {
         const { options } = interation;
         const day = options.getInteger('day')
         await interation.reply (`the session is set for ${day}`);
-    },
+    }
 };
